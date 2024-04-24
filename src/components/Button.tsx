01ce890505ca87f7c7bb21cb84e5969acda0afc5
Dan Moffat <danmoffat@gmail.com>
2024-04-24 01:59:33 +0100

    Another refactor, that pesky el prop...

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -12,19 +12,19 @@ type LinkProps = PropsWithChildren<{
   ComponentPropsWithoutRef<'a'>;
 
 const Button = (props: ButtonProps | LinkProps) => {
-  const { el } = props;
-
-  switch (el) {
-    case 'button':
-    default:
-      return (
-        <button className='border-2 border-sky-800 bg-sky-600 py-2 hover:bg-sky-500'>
-          {props.children}
-        </button>
-      );
-    case 'anchor':
-      return <a {...props}>{props.children}</a>;
+  if (props.el === 'anchor') {
+    const { children, ...rest } = props;
+    return <a {...rest}>{children}</a>;
   }
+
+  const { children, ...rest } = props;
+  return (
+    <button
+      className='border-2 border-sky-800 bg-sky-600 py-2 hover:bg-sky-500'
+      {...rest}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
